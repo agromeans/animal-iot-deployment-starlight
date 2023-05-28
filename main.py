93b10a9a9e37c48995292741b404c24f1dc2b341
agromeans <agromeans@gmail.com>
2023-05-28 12:47:59 +0800

    update package

--- main.py
+++ main.py
@@ -3,47 +3,64 @@ from lib.logger_opt import *
 import lib.opt as opt
 import lib.util_opt as util
 import config
-import subprocess
 import os
-import shutil
 import argparse
 
 def process_command():
     parser = argparse.ArgumentParser()
     parser.add_argument('-server', '--server', type=str, required=True, help='api server ip port')
-    parser.add_argument('-repo', '--repo', type=str, required=True, help='git repod')
     return parser.parse_args()
     
 def run(args):
-    # 1. 使用shell啟動python程式，在crontab內設定觸發shell頻率(1hr)，透過參數來區分要升級的服務
-    util.delete_folder_anyway(os.path.join(config.path_root, args.repo))
-    util.delete_file_anyway(os.path.join(config.path_root, f"{args.repo}.sh"))
-    
-    # 2. 取出frp資訊向伺服器取得token與github ip(更新github.txt)，若有token則繼續下一步，否則停止此次流程
-    # 2.1. get token and github
-    token, github = opt.get_info_from_server(args.server)
-    if not token or not github:
-        return
-        
-    # 2.2. save token
+    # 1. check and save token
+    token, _ = opt.get_info_from_server(args.server)
+    if token is None:
+        logger.error('device token is null, end process')
     util.save_token(token)
     
-    # 3. git clone對應repo到根目錄下，從repo中取出部署用shell
-    # 3.1. git clone to root/{repo}, check dir and shell
-    subprocess.run(['git', 'clone', f"git@{github}:agromeans/{args.repo}.git", f"{os.path.join(config.path_root, args.repo)}"], check=True)
+    # 2. get version list from server api
+    verison_list = opt.get_version_from_server(args.server)
     
-    # 3.2. check dir and shell
-    if not os.path.isdir(os.path.join(config.path_root, args.repo)):
-        logger.error('folder not exist', exc_info=True)
-        return
+    # 3. get the version from itself services
+    for item in verison_list:
+        service_name = item.get('service_name')
+        new_version = item.get('version')
         
-    if not os.path.exists(os.path.join(config.path_root, args.repo, 'docker-deployment.sh')):
-        logger.error('deployment shell not exist', exc_info=True)
-        return
+        os.system(f"docker exec {service_name} cat agromeans/config.ini > {service_name}.txt")
+        itself_version = util.read_config_value(f"{service_name}.txt", 'common', 'version').strip()
         
-    # 3.3. move deployment shell and docker-compose yml(搬移時改名為$repo)
-    shutil.move(os.path.join(config.path_root, args.repo, 'docker-deployment.sh'), os.path.join(config.path_root, f"{args.repo}.sh"))
-    
+        # 4. check the version is equal
+        if itself_version != new_version:
+            # 5. get hub url if the version is not equal
+            hub_info, mkdir_list = opt.get_hub_info_from_server(args.server, service_name)
+            hub_name = hub_info.split('/')[-1].replace('.git', '')
+            repo_name = hub_name.replace('-hub', '')
+            
+            # 6. remove hub and itself_version.txt
+            util.delete_folder_anyway(hub_name)
+            
+            # 7. madir for folders
+            for _dir in mkdir_list:
+                os.system(f"mkdir -p {_dir}")
+                
+            # 8. git clone hub, cd in the hub
+            os.system(f"git clone --depth 1 {hub_info}")
+            
+            # 9. tar -zxvf package
+            os.system(f"cd {hub_name} && tar -zxvf package.tar.gz")
+            
+            # 10. remove docker image and container
+            os.system(f"docker rm -f {service_name}")
+            os.system(f"docker rmi {service_name}")
+            
+            # 11. docker build and docker-compose up
+            os.system(f"docker build -t {service_name} {hub_name}/{repo_name}/ --no-cache")
+            os.system(f"/usr/local/bin/docker-compose -f {hub_name}/{repo_name}/docker-compose.yml up -d")
+            
+            # 12. remove hub and itself_version.txt
+            util.delete_folder_anyway(hub_name)
+            
+        util.delete_file_anyway(f"{service_name}.txt")
 if __name__ == "__main__":
     args = process_command()
     config.reload_config()
